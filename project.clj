01f69ed26f7d977a60e85dc8759558bee276ebf7
Joe Wass <jwass@crossref.org>
2016-12-01 14:22:28 +0000

    v0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-event-bus "0.1.0"
+(defproject event-data-event-bus "0.1.1"
   :description "Event Bus for Event Data"
   :url "http://eventdata.crossref.org/"
   :license {:name "MIT License"
