b733595e9a2b4980e99a3c0d4c7a8639a87d2acb
Joe Wass <jwass@crossref.org>
2016-12-09 15:49:29 +0000

    Bump v0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-event-bus "0.1.2"
+(defproject event-data-event-bus "0.1.3"
   :description "Event Bus for Event Data"
   :url "http://eventdata.crossref.org/"
   :license {:name "MIT License"
